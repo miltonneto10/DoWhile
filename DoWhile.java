9ad842099a5f3296b49cd842e15a952a2b5cd009
Milton Salustiano <109237764+miltonneto10@users.noreply.github.com>
2023-08-06 12:20:52 -0300

    Commit Inicial

---
+++ DoWhile.java
@@ -0,0 +1,21 @@
+//FLUXO DE REPETIÇÃO
+//Utilização do  DO...WHILE (FAÇA...ENQUANTO)
+	
+import java.util.arrayList; //importando a classe ArrayList para poder fazer uso do vetor e armazenar elementos
+
+public classs DoWhile {
+	/*Método principal da classe*/
+	public static void main (String [] args) { //Fluxo de repetição usando While
+		int i = 5;
+		do {
+			System.out.println(i);//imprima o valor de i
+			i++;//incremento da variável i
+		}While (i < 3) {
+			
+		}//enquanto i for menor que 3
+	}//fim do método
+}//fim da classe
+
+/**
+A estrutura do...while, por sua vez, inicia a execução da instrução, imprimindo e incrementando o valor da variável “i”, enquanto este for verdadeiro.
+**/
